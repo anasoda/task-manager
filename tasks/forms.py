7ab5059f256c52d624aa488f145539fc198a8d69
Anas Ouda <anasoda.pro@gmail.com>
2026-07-08 15:56:56 +0300

    editing date widget

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,3 +6,6 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["title", "description", "due_date", "is_completed"]
+        widgets = {
+            "due_date": forms.DateInput(attrs={"type": "date"}),
+        }
